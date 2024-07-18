7a7de8682f44455cd141711368efa1c0b18614c4
amisha <amishamalviya2607@gmail.com>
2024-07-18 13:26:19 +0530

    Fix mobile toggle menu functionality by adding toggle and toggleNav functions, and updating event listeners

--- controller.js
+++ controller.js
@@ -1,11 +1,11 @@
 window.onload = () => {
-  const navMenu = document.querySelector('.nav-menu');
-  const navItems = document.querySelectorAll('.nav-item');
-  const hamburger = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav-menu'); // Get the navigation menu element
+  const navItems = document.querySelectorAll('.nav-item'); // Get all navigation items
+  const hamburger = document.querySelector('.nav-toggle'); // Get the hamburger menu icon
   
   const toggle = e => e.classList.toggle('is-active');
   const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
 
-  hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
+  hamburger.addEventListener('click', () => toggle(navMenu));//Did Changes here(removed the in-active)
   Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 }
